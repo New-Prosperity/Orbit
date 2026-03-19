7392eb8bda4af049e8ec09c2d4e224c6cbb94906
Kalou <lucas.boreas1@gmail.com>
2026-03-19 18:08:00 +0100

    Revert to working state 6a2226b

--- src/main/kotlin/me/nebula/orbit/utils/customcontent/armor/ArmorPart.kt
+++ src/main/kotlin/me/nebula/orbit/utils/customcontent/armor/ArmorPart.kt
@@ -31,7 +31,7 @@ sealed class ArmorPart(
         Vec(signX * cx + tbnOffsetX, signZ * cz + tbnOffsetY, signY * cy + tbnOffsetZ)
 
     fun convertPivot(px: Double, py: Double, pz: Double): Vec =
-        convertCenter(px, py, pz)
+        Vec(px, -pz, py)
 
     fun alternateLayerPart(): ArmorPart? = when (this) {
         Chestplate -> InnerArmor
