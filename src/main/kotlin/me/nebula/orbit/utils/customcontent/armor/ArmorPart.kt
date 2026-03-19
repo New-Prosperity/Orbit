2f5295014c255e3437d18563d03f1e9b0fd626fb
Kalou <lucas.boreas1@gmail.com>
2026-03-19 17:51:30 +0100

    Add offsets to pivot to cancel them in pos-piv diff (reduces rotation leakage)

--- src/main/kotlin/me/nebula/orbit/utils/customcontent/armor/ArmorPart.kt
+++ src/main/kotlin/me/nebula/orbit/utils/customcontent/armor/ArmorPart.kt
@@ -31,7 +31,7 @@ sealed class ArmorPart(
         Vec(signX * cx + tbnOffsetX, signZ * cz + tbnOffsetY, signY * cy + tbnOffsetZ)
 
     fun convertPivot(px: Double, py: Double, pz: Double): Vec =
-        Vec(px, -pz, py)
+        Vec(px + tbnOffsetX, -pz + tbnOffsetY, py + tbnOffsetZ)
 
     fun alternateLayerPart(): ArmorPart? = when (this) {
         Chestplate -> InnerArmor
