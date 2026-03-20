97b2799abee676317e4bec287a6c6bf1f75eb71a
Kalou <lucas.boreas1@gmail.com>
2026-03-20 21:15:28 +0100

    GUI: moderate model-space XYZ expansion with raw corners (not scaled by 32)

--- src/main/resources/shaders/armor/include/cem/vert_setup.glsl
+++ src/main/resources/shaders/armor/include/cem/vert_setup.glsl
@@ -2,17 +2,15 @@ vec4 modelPos = vec4(pos, 1.0);
 modelPos.xyz += vec3(ProjMat[3].xy / vec2(ProjMat[0][0], ProjMat[1][1]), 0) * mat3(ModelViewMat);
 
 vec2 cornerT = corner * 2 - 1;
-if (ProjMat[3][0] == -1)
-    cornerT = cornerT.yx * 32;
-
 vec4 cem_Pos;
 if (ProjMat[3][0] == -1) {
-    vec4 expandedPos = modelPos;
-    float guiExp = 2.5 * cem_size;
-    expandedPos.x += cornerT.x * guiExp;
-    expandedPos.y += cornerT.y * guiExp;
-    expandedPos.z += cornerT.x * guiExp;
-    cem_Pos = ModelViewMat * expandedPos;
+    float ge = 10.0 * cem_size;
+    vec4 ep = modelPos;
+    ep.x += cornerT.x * ge;
+    ep.y += cornerT.y * ge;
+    ep.z += (cornerT.x - cornerT.y) * ge * 0.5;
+    cem_Pos = ModelViewMat * ep;
+    cornerT = cornerT.yx * 32;
 } else {
     cem_Pos = ModelViewMat * modelPos + vec4(cornerT * 2.5 * cem_size, 0, 0);
 }
